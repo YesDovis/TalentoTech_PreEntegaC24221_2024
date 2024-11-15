0f5a688ec07141a07e8ec8741fe1abc115fd76e5
yesdovis <dovisyesica@gmail.com>
2024-11-14 23:53:42 -0300

    menu opciones

---
+++ inventario.py
@@ -0,0 +1,12 @@
+
+opcion="" 
+while opcion != 6:                                 
+    print("Bienvenidos a 'La Birra es Bella'\n")
+    print("Control de Stock\n")
+    print("1. Alta de un nuevo producto.")
+    print("2. Consultar productos en stock.")
+    print("3. Modificar stock de un producto.")
+    print("4. Eliminar un producto (Proximamente).")
+    print("5. Listar productos con bajo stock (Proximamente).")
+    print("6. Salir del sistema.")
+    opcion = int(input("Ingrese la opcion que desea usar: "))
